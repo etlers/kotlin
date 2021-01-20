fdad1a5599335b66a3f7a9a381768e72b29b6f5f
etlers <etlersadm@gmail.com>
2021-01-20 20:22:34 +0900

    just description

---
+++ lecture_12.kt
@@ -0,0 +1,18 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+// 패키지 선언. 접근 제한자에도 사용하게 됨
+package com.etlers.base
+// 패키지가 다르면 다른 패키지의 함수, 변수 등을 사용할 수 없음
+// import를 사용해야 함
+import com.youtube.etlers.base
+// 클래스명과 파일명이 일치하지 않아도 됨
+// 하나의 파일에 여러개의 클래스를 넣어도 됨. 파일 내의 패키지 키워드를 기준으로 처리하기 때문
+// 미선언신 장동으로 "default" 패키지로 묶이게 됨
+
+// 기본 프로젝트 구조
+fun main() {
+    
+}
